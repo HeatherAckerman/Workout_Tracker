dddc986640150652e5dc44174d46e03d0e0d9966
HeatherAckerman <hla20115213@gmail.com>
2020-11-11 10:37:50 -0600

    basic server working

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,21 @@
 //Require express and mongoose
+const express = require("express");
+const mongoose = require("mongoose");
 
 //Create port
+const PORT = process.env.PORT || 3000;
 
 //Use express/urlencoded/json/static
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 //Use mongoose
 
 //Create routes
 
 //Have server listen
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
